5bb2997ac21f064462fd0cd53c498197424fb3b4
kochambercommerce.github.io <https://github.com/ron981010/kochambercommerce.github.io.gits>
2021-04-07 20:37:54 -0400

    goooo

---
+++ js/events.js
@@ -0,0 +1,9 @@
+var jcontent = {
+
+    "events": "February 29: Photography Classes",
+    "vad" : "October 15-16: Kermes new restaurant"
+    
+}
+  
+  var output = document.getElementById('output');
+  output.innerHTML = jcontent.events;
