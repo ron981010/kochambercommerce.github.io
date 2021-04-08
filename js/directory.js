cc1d9be684b5cd15a30567b3eb71d2f36afec896
kochambercommerce.github.io <https://github.com/ron981010/kochambercommerce.github.io.gits>
2021-04-08 01:46:40 -0400

    go last

---
+++ js/directory.js
@@ -0,0 +1,51 @@
+const requestURL = "https://ron981010.github.io/kochambercommerce.github.io/directory.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //   console.table(jsonObject); //temporary checking for valid response and data parsing
+
+    const business = jsonObject["business"];
+    const home = business.filter(
+      (busines) =>
+        busines.name == "Monopol" ||
+        busines.name == "Miraflores" ||
+        busines.name == "Cine Center" ||
+        busines.name == "BNB" ||
+        busines.name == "BoA" ||
+        busines.name == "Finor" ||
+        busines.name == "Soboce" 
+    );
+   
+    home.forEach((busines) => {
+      let card = document.createElement("article");
+      let stat = document.createElement("info");
+      let h3 = document.createElement("h3");
+      let h4 = document.createElement("h4");
+      const p1 = document.createElement("p");
+      const p2 = document.createElement("p");
+      const p3 = document.createElement("p");
+
+      h3.innerHTML = `${busines.name}`;
+      h4.innerHTML = `${busines.logo}`;
+      p1.textContent = `${"contact info:"}  ${busines.contactinfo}`;
+      p2.textContent = `web link:  ${busines.weblink}`;
+    
+      const image = document.createElement("img");
+
+      image.setAttribute("src", "images/" + busines.photo);
+      image.setAttribute("alt", busines.photo);
+
+      card.appendChild(stat);
+      card.appendChild(image);
+      document.querySelector("div.cards").appendChild(card);
+
+      stat.appendChild(h3);
+      stat.appendChild(h4);
+      stat.appendChild(p1);
+      stat.appendChild(p2);
+      stat.appendChild(p3);
+    });
+  });
